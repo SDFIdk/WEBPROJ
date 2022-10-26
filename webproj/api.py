684593320f31f1d42f1951a42021660fba2e11b0
Kristian Evers <kristianevers@gmail.com>
2022-10-26 10:22:35 +0200

    bump version to 1.2.1

--- webproj/api.py
+++ webproj/api.py
@@ -11,7 +11,7 @@ from pyproj.transformer import Transformer, AreaOfInterest
 
 from webproj.utils import IntFloatConverter
 
-version = "1.2.0"
+version = "1.2.1"
 
 if "WEBPROJ_LIB" in os.environ:
     pyproj.datadir.append_data_dir(os.environ["WEBPROJ_LIB"])
